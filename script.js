bfaf6a13042699ae0b928b079bb8765f29fcc587
bernyig <bernardommguerreiro@gmail.com>
2023-07-31 10:50:21 +0200

    createElement swaped innerHTML, readStatus event swaped toggleRead()

--- script.js
+++ script.js
@@ -26,15 +26,6 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.toggleRead = function () {
-  this.read = !this.read;
-};
-
-function toggleRead(index) {
-  myLibrary[index].toggleRead();
-  displayLibrary();
-}
-
 function addBookToLibrary() {
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
@@ -50,21 +41,64 @@ function displayLibrary() {
   libraryContainer.innerHTML = "";
   for (let i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i];
+
     let bookCard = document.createElement("div");
     bookCard.setAttribute("class", "book-card");
-    bookCard.innerHTML = `
-    <div>
-      <p>Title: <strong>${book.title}</strong></p>
-      <p>Author: <strong>${book.author}</strong></p>
-      <p>Pages: <strong>${book.pages}</strong></p>
-      <p>${book.read ? "Read" : "Not read"}</p>
-    </div>
-    <div>
-    <button id="toggle-read-btn" onclick="toggleRead(${i})">Read/Not Read</button>
-    <button id="remove-btn" onclick="removeBook(${i})">X</button>
-    <div>`;
 
+    const leftDiv = document.createElement("div");
+    leftDiv.setAttribute("class", "left-div");
+
+    const titleParagraph = document.createElement("p");
+    titleParagraph.setAttribute("class", "title");
+    titleParagraph.innerHTML = `Title: <strong>${book.title}</strong>`;
+
+    const authorParagraph = document.createElement("p");
+    authorParagraph.setAttribute("class", "author");
+    authorParagraph.innerHTML = `Author: <strong>${book.author}</strong>`;
+
+    const pagesParagraph = document.createElement("p");
+    pagesParagraph.setAttribute("class", "pages");
+    pagesParagraph.innerHTML = `Pages: <strong>${book.pages}</strong>`;
+
+    const readStatus = document.createElement("button");
+    readStatus.className = book.read ? "read-confirmed" : "read-declined";
+    readStatus.textContent = book.read ? "Read" : "Not read";
+
+    const rightDiv = document.createElement("div");
+    rightDiv.setAttribute("class", "right-div");
+
+    const removeBtn = document.createElement("button");
+    removeBtn.setAttribute("id", "remove-btn");
+    removeBtn.innerHTML = "X";
+
+    leftDiv.appendChild(titleParagraph);
+    leftDiv.appendChild(authorParagraph);
+    leftDiv.appendChild(pagesParagraph);
+    leftDiv.appendChild(readStatus);
+
+    rightDiv.appendChild(removeBtn);
+
+    bookCard.appendChild(leftDiv);
+    bookCard.appendChild(rightDiv);
     libraryContainer.appendChild(bookCard);
+
+    removeBtn.addEventListener("click", function () {
+      removeBook(i);
+    });
+
+    readStatus.addEventListener("click", function () {
+      book.read = !book.read;
+
+      if (book.read) {
+        readStatus.classList.remove("read-declined");
+        readStatus.classList.add("read-confirmed");
+        readStatus.textContent = "Read";
+      } else {
+        readStatus.classList.remove("read-confirmed");
+        readStatus.classList.add("read-declined");
+        readStatus.textContent = "Not read";
+      }
+    });
   }
 }
 
