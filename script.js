8ba7e986675d72829737c6f05ee157a7f80e7bbf
bernyig <bernardommguerreiro@gmail.com>
2023-08-10 22:37:34 +0200

    improve js code readability

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const openFormBtn = document.getElementById("openFormBtn");
 const form = document.getElementById("form");
 const libraryContainer = document.getElementById("libraryContainer");
 
+openFormBtn.addEventListener("click", function () {
+  if (form.style.display === "none") {
+    form.style.display = "flex";
+    openFormBtn.innerHTML = '<img src="img/minus-circle.svg" />';
+  } else {
+    form.style.display = "none";
+    openFormBtn.innerHTML = '<img src="img/plus-circle.svg" />';
+  }
+});
+
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
@@ -11,14 +21,32 @@ class Book {
   }
 }
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  getBookInfo();
+  form.reset();
+  form.style.display = "none";
+  openFormBtn.innerHTML = '<img src="img/plus-circle.svg" />';
+});
+
+function getBookInfo() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").checked;
+
+  library.createBook(title, author, pages, read);
+}
+
 class Library {
   constructor() {
     this.books = [];
   }
 
-  addBook(title, author, pages, read) {
+  createBook(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     this.books.push(newBook);
+    this.display();
   }
 
   removeBook(index) {
@@ -35,49 +63,47 @@ class Library {
       const bookCard = document.createElement("div");
       bookCard.setAttribute("class", "book-card");
 
-      const leftDiv = document.createElement("div");
-      leftDiv.setAttribute("class", "left-div");
+      const leftSide = document.createElement("div");
+      leftSide.setAttribute("id", "leftSide");
 
-      const titleParagraph = document.createElement("p");
-      titleParagraph.setAttribute("class", "title");
-      titleParagraph.innerHTML = `Title: <strong>${book.title}</strong>`;
+      const cardTitle = document.createElement("p");
+      cardTitle.setAttribute("id", "cardTitle");
+      cardTitle.innerHTML = `Title: ${book.title}`;
 
-      const authorParagraph = document.createElement("p");
-      authorParagraph.setAttribute("class", "author");
-      authorParagraph.innerHTML = `Author: <strong>${book.author}</strong>`;
+      const cardAuthor = document.createElement("p");
+      cardAuthor.setAttribute("id", "cardAuthor");
+      cardAuthor.innerHTML = `Author: ${book.author}`;
 
-      const pagesParagraph = document.createElement("p");
-      pagesParagraph.setAttribute("class", "pages");
-      pagesParagraph.innerHTML = `Pages: <strong>${book.pages}</strong>`;
+      const cardPages = document.createElement("p");
+      cardPages.setAttribute("id", "cardPages");
+      cardPages.innerHTML = `Pages: ${book.pages}`;
 
-      const readStatus = document.createElement("button");
-      readStatus.className = book.read ? "read-confirmed" : "read-declined";
-      readStatus.textContent = book.read ? "Read" : "Not read";
+      const cardRead = document.createElement("button");
+      cardRead.className = book.read ? "read-confirmed" : "read-declined";
+      cardRead.innerHTML = book.read ? "Read" : "Not Read";
 
-      const rightDiv = document.createElement("div");
-      rightDiv.setAttribute("class", "right-div");
+      const rightSide = document.createElement("div");
+      rightSide.setAttribute("id", "rightSide");
 
-      const removeBtn = document.createElement("button");
-      removeBtn.setAttribute("id", "remove-btn");
-      removeBtn.innerHTML = "X";
+      const cardRemove = document.createElement("button");
+      cardRemove.setAttribute("id", "removeBtn");
+      cardRemove.innerHTML = "X";
 
-      leftDiv.appendChild(titleParagraph);
-      leftDiv.appendChild(authorParagraph);
-      leftDiv.appendChild(pagesParagraph);
-      leftDiv.appendChild(readStatus);
-
-      rightDiv.appendChild(removeBtn);
-
-      bookCard.appendChild(leftDiv);
-      bookCard.appendChild(rightDiv);
       libraryContainer.appendChild(bookCard);
+      bookCard.appendChild(leftSide);
+      bookCard.appendChild(rightSide);
+      leftSide.appendChild(cardTitle);
+      leftSide.appendChild(cardAuthor);
+      leftSide.appendChild(cardPages);
+      leftSide.appendChild(cardRead);
+      rightSide.appendChild(cardRemove);
 
-      removeBtn.addEventListener("click", () => {
+      cardRemove.addEventListener("click", () => {
         this.removeBook(index);
         this.display();
       });
 
-      readStatus.addEventListener("click", () => {
+      cardRead.addEventListener("click", () => {
         this.toggleReadStatus(index);
         this.display();
       });
@@ -85,32 +111,4 @@ class Library {
   }
 }
 
-const myLibrary = new Library();
-
-openFormBtn.addEventListener("click", () => {
-  if (form.style.display === "none") {
-    form.style.display = "flex";
-    openFormBtn.innerHTML = '<img src="img/minus-circle.svg"/>';
-  } else {
-    form.style.display = "none";
-    openFormBtn.innerHTML = '<img src="img/plus-circle.svg"/>';
-  }
-});
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  addBookToLibrary();
-  form.reset();
-});
-
-function addBookToLibrary() {
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const read = document.querySelector("#read").checked;
-
-  myLibrary.addBook(title, author, pages, read);
-  myLibrary.display();
-}
-
-myLibrary.display();
+const library = new Library();
